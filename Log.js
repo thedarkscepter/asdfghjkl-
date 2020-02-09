4355003d73368e4a8dd27a7c28ba28f2a95bab77
thedarkscepter <60280897+thedarkscepter@users.noreply.github.com>
2020-02-09 09:40:59 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,29 @@
+class Log {
+    constructor(x, y, height, angle) {
+      this.image = loadImage("sprites/wood2.png");
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 20, height, options);
+      this.width = 20;
+      this.height = height;
+      Matter.Body.setAngle(this.body, angle);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      strokeWeight(4);
+      stroke("green");
+      fill(255);
+      image(this.image,0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# asdfghjkl-
-qwertyuiop
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
